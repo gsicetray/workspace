c3418d1a7607b2d7d76e8d2706ceda2486033948
Grant <gsicetray@gmail.com>
2013-02-24 15:10:57 -0500

    Birdwatching
    2nd iOS ap

---
+++ BirdWatching/BirdWatching/BirdsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BirdsAppDelegate.h
+//  BirdWatching
+//
+//  Created by Grant Summers on 2/24/13.
+//  Copyright (c) 2013 Grant Summers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BirdsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BirdWatching/BirdWatching/BirdsMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  BirdsMasterViewController.h
+//  BirdWatching
+//
+//  Created by Grant Summers on 2/24/13.
+//  Copyright (c) 2013 Grant Summers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BirdsMasterViewController : UITableViewController
+
+@end
